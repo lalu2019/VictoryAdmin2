548b2fe208d4c4a46748ba770ad36edc9eaee084
Lalu <mapparena@gmail.com>
2022-02-19 22:37:02 +0530

    Auto-generated commit [ci skip]

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,6 @@
+// importScripts('https://www.gstatic.com/firebasejs/7.6.0/firebase-app.js');
+// importScripts('https://www.gstatic.com/firebasejs/7.6.0/firebase-messaging.js');
+//   firebase.initializeApp({
+//     messagingSenderId: "280549349717",
+// });
+//   const messaging = firebase.messaging();
